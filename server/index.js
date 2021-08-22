662973d0c865419f89d0289c6b5967910311fa37
aymeeflores <aymeeflores@users.noreply.github.com>
2021-08-21 22:34:59 -0400

    api functionality

--- server/index.js
+++ server/index.js
@@ -1,29 +1,38 @@
-const express = require('express');
-const db = require('./database');
+const express = require("express");
+const db = require("./database");
 const app = express();
+const cors = require("cors");
 
+var corOptions = {
+  origin: "*",
+  optionSuccessStatus: 200,
+};
+
+app.use(cors());
 app.use(express.json());
 
 // middlewares
-const valid_token = require('./middleware/valid_token');
-const is_auth = require('./middleware/is_auth');
+const valid_token = require("./middleware/valid_token");
+const is_auth = require("./middleware/is_auth");
 
 // routes import
-const loginRoutes = require('./routes/login');
-const locationRoutes = require('./routes/location');
-const messageRoutes = require('./routes/messages');
+const loginRoutes = require("./routes/login");
+const locationRoutes = require("./routes/location");
+const messageRoutes = require("./routes/messages");
 
 // default
-app.get('/', (req, res) => { res.json({message: 'hello from api'}) });
+app.get("/", (req, res) => {
+  res.json({ message: "hello from api" });
+});
 // static files :: avatars
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // routes use
-app.use('/login', loginRoutes);
-app.use('/messages', valid_token, is_auth,  messageRoutes);
-app.use('/location', valid_token, is_auth, locationRoutes);
+app.use("/login", loginRoutes);
+app.use("/messages", valid_token, is_auth, messageRoutes);
+app.use("/location", valid_token, is_auth, locationRoutes);
 
 // open server
-app.listen(3000, () => {
-	console.log( " Listening on por 3000 ");
+app.listen(8080, () => {
+  console.log("Listening on por 8080");
 });
