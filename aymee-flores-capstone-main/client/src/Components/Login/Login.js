3a7e1728dfb82aa8aa912d2772ba312e25bb7182
aymeeflores <aymee.flores@gmail.com>
2022-06-18 16:20:21 -0400

    login style refactor

--- aymee-flores-capstone-main/client/src/Components/Login/Login.js
+++ aymee-flores-capstone-main/client/src/Components/Login/Login.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import { Form, Button, FormGroup } from "react-bootstrap";
 import "./Login.scss";
 import Constants from "../../constants";
+
 import axios from "axios";
 
 export default class Login extends Component {
@@ -43,56 +45,53 @@ export default class Login extends Component {
 		if (!this.state.user) {
 			return (
 				<>
-					<form className="mainlogin">
-						<div className=" row">
-							<h1 className=" mainlogin__greeting">Welcome Back!</h1>
-							<h3 className=" mainlogin__subtitle">Connect with your neighbors</h3>
-						</div>
+					<div className="loginwrapper">
+						<Form className="mainlogin">
+							<div className=" row">
+								<h1 className=" mainlogin__greeting">Welcome Back!</h1>
+								<h3 className=" mainlogin__subtitle">Connect with your neighbors</h3>
+							</div>
 
-						<div className="logincontainer">
-							<label htmlFor="exampleInputEmail1" className="logincontainer__loginlabel">
-								Email address
-							</label>
-							<input
-								type="email"
-								className="logininput"
-								id="exampleInputEmail1"
-								aria-describedby="emailHelp"
-								placeholder="Enter email"
-								onChange={(e) =>
-									this.setState({
-										login: { ...this.state.login, email: e.target.value },
-									})
-								}
-								defaultValue={this.state.login.email}
-							/>
-						</div>
-						<div className="logincontainer ">
-							<label htmlFor="exampleInputPassword1" className="logincontainer__loginlabel">
-								Password
-							</label>
-							<input
-								type="password"
-								className="logininput"
-								id="exampleInputPassword1"
-								placeholder="Password"
-								onChange={(e) =>
-									this.setState({
-										login: { ...this.state.login, code: e.target.value },
-									})
-								}
-								defaultValue={this.state.login.code}
-							/>
-						</div>
-						<button
-							type="button"
-							className="loginbtn"
-							// type="button"
-							onClick={this.doLogin}
-						>
-							Log In
-						</button>
-					</form>
+							<FormGroup className="logincontainer">
+								<label htmlFor="exampleInputEmail1" className="logincontainer__loginlabel">
+									Email address
+								</label>
+								<input
+									type="email"
+									className="logininput"
+									id="exampleInputEmail1"
+									aria-describedby="emailHelp"
+									placeholder="Enter email"
+									onChange={(e) =>
+										this.setState({
+											login: { ...this.state.login, email: e.target.value },
+										})
+									}
+									defaultValue={this.state.login.email}
+								/>
+							</FormGroup>
+							<FormGroup className="logincontainer ">
+								<label htmlFor="exampleInputPassword1" className="logincontainer__loginlabel">
+									Password
+								</label>
+								<input
+									type="password"
+									className="logininput"
+									id="exampleInputPassword1"
+									placeholder="Password"
+									onChange={(e) =>
+										this.setState({
+											login: { ...this.state.login, code: e.target.value },
+										})
+									}
+									defaultValue={this.state.login.code}
+								/>
+							</FormGroup>
+							<Button type="Button" className="loginbtn" onClick={this.doLogin}>
+								Log In
+							</Button>
+						</Form>
+					</div>
 				</>
 			);
 		}
