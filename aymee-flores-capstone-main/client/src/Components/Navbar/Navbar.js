9a09d1273cc98361c01bdd479f61997e6d0fdf97
aymeeflores <aymee.flores@gmail.com>
2023-05-18 22:47:21 -0400

    started navbar refactoring

--- aymee-flores-capstone-main/client/src/Components/Navbar/Navbar.js
+++ aymee-flores-capstone-main/client/src/Components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Navbar.scss";
-import Avatar from "../../assets/avatar.jpg";
+// import Avatar from "../../assets/avatar.jpg";
 import { Link } from "react-router-dom";
 import Dropdown from "react-bootstrap/Dropdown";
 import { LinkContainer } from "react-router-bootstrap";
@@ -33,11 +33,8 @@ class Navbar extends React.Component {
 				<nav className="navbar">
 					<div className="container-fluid">
 						<LinkContainer to="/">
-							<div className="navbar-brand">Resident Social</div>
+							<div className="navbar__brand">Resident Social</div>
 						</LinkContainer>
-						<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-							<span className="navbar-toggler-icon"></span>
-						</button>
 
 						<div className="collapse navbar-collapse" id="navbarColor01">
 							<ul className="navbar-nav me-auto">
